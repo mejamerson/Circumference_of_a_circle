e29eb45475f5faa4073a312d876fa93e5a3c0ecb
Maurice <Maurice@MAURICE-PC>
2017-06-13 13:17:52 -0500

    Add .gitignore and .gitattributes.

---
+++ Circumference of a circle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circumference_of_a_circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+            // Prompt the user to enter the radius of a circle.
+        {
+            Console.WriteLine("Please write the radius of your circle and press enter.");
+            // Circumference of a Circle C= 2 * pi * r
+             double pi =  3.14;
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(" The circumference of the circle is " + (2 * pi * radius));
+            Console.WriteLine(" The area of the circle is " + (pi * radius * radius));
+            Console.ReadLine();
+             
+
+                 
+        }
+    }
+}
